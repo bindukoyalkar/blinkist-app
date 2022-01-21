bb849d0f9afe380ca4e9f712d357d78a2e348cbb
bindukoyalkar <bindu.koyalkar@zemosolabs.com>
2022-01-21 17:29:42 +0530

    added jest dom

--- src/atoms/Icons/__tests__/Icons.test.tsx
+++ src/atoms/Icons/__tests__/Icons.test.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import "@testing-library/jest-dom"
 import Icons from '../Icons';
 import {render,screen } from '@testing-library/react';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
